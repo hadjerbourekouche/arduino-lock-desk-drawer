3f0b6ddd9dd8c8fd3065fb52b36497d1e116ce0a
Joey Bronner <joeybronner@gmail.com>
2017-05-07 23:55:11 +0200

    First version code

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
+
+![Prototype](img/proto.png)
+
 # Arduino Lock Desk Drawer
 > arduino, locker, fingerprint, sensor, drawer, step motor
 
-Arduino locker for your desk drawer using fingerprint sensor &amp; step motor
+Arduino locker for your desk drawer using fingerprint sensor &amp; buzzer &amp; servo.
 
 ## Features
 
@@ -10,5 +13,11 @@ TODO
 ## Getting started
 
 TODO
+
+## Useful links
+
+ - [Enrolling fingerprint](https://learn.adafruit.com/adafruit-optical-fingerprint-sensor/enrolling-with-arduino)
+ - [Fingerprint element](http://tinkersphere.com/sensors/1241-fingerprint-scanner-sensor-arduino-compatible.html)
+ - [Buzzer: Use tone() with Arduino to make noise](https://programmingelectronics.com/an-easy-way-to-make-noise-with-arduino-using-tone/)
 
 
--- arduino-lock-desk-drawer/arduino-lock-desk-drawer.ino
+++ arduino-lock-desk-drawer/arduino-lock-desk-drawer.ino
@@ -1,9 +1,133 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+#include <Stepper.h>
 
+// FINGERPRINT
+int getFingerprintIDez();
+SoftwareSerial mySerial(2, 3); // pin #2 is IN from sensor (GREEN wire) pin #3 is OUT from arduino  (WHITE wire)
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+// SERVO
+Servo servo;
+
+// BUZZER
+int buzzer = 7;
+
+void setup() {                
+  Serial.begin(9600);
+
+  // Init fingerprint
+  finger.begin(57600);
+
+  // Init servo
+  servo.attach(8);
+  servo.write(30);
+
+  // Init buzzer
+  pinMode(buzzer,OUTPUT);
+
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  Serial.println("Waiting for valid finger...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int idFinger = getFingerprintIDez();
+  if (idFinger >= 0) {
+     tone(buzzer, 1000, 150);
+     delay(200);
+     tone(buzzer, 1000, 150);
+     servo.write(120);
+     delay(5000);
+     servo.write(30);
+  }
+  delay(100);
 }
+
+/*
+ * Fingerprint Utils
+ */
+ 
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+}
+
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID; 
+}
+
